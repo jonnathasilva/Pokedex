3fbc5f8fb75984d7581d05a2f363e3a75f168f03
jonnathan silva <sjonnathan928@gmail.com>
2022-12-22 10:37:03 -0300

    icon logo

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,7 +9,7 @@ export const Card: React.FC<Props> = ({ url }) => {
   const [searchParams] = useSearchParams()
   const { data, isLoading } = usePokemon(url)
 
-  if (isLoading || !data?.sprites.front_default) {
+  if (isLoading) {
     return (
       <div data-testid="loading" className="transform">
         <div className="border-t-transparent border-solid animate-spin rounded-full border-black-400 border-8 h-20 w-20"></div>
@@ -22,7 +22,7 @@ export const Card: React.FC<Props> = ({ url }) => {
       to={`/pokemon/?name=${data?.name}&page=${searchParams.get("page") || 1}`}
       className={`w-full h-32 flex flex-col justify-center items-center rounded-lg ease-in hover:scale-105 ${data?.types[0].type.name}`}
     >
-      <img src={data?.sprites.front_default} alt={data?.name} />
+      <img loading="lazy" src={data?.sprites.front_default} alt={data?.name} />
       <p className="text-white capitalize">{data?.name}</p>
     </Link>
   )
